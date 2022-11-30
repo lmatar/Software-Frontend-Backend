8ec2ebabf87ad1a6a30c0cbcc53080e6780a069d
Christopher <32846661+clee93@users.noreply.github.com>
2022-11-30 09:43:08 -0600

    Updated Models
    + Modified models.py
    + Modified Index.html to support new models
    + Migrated changes to db.sqlite3, original myapp_sensors_table has been overridden.
    > please verify that original functionality is still maintained i.e., check that other database tables have not been unintentionally modified, and that original code functionality works as desired.

--- IPRO_SOTERIA/myapp/models.py
+++ IPRO_SOTERIA/myapp/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 class sensors_table(models.Model):
-    sensor_id = models.CharField(max_length=32, blank=True)
+    ip_id = models.CharField(max_length=64, blank=True, primary_key=True)
+    sensor = models.CharField(max_length=32, blank=True)
     value = models.CharField(max_length=16,null=True)
     time = models.CharField(max_length=64,null=True)
     status = models.CharField(max_length=64,null=True) 
-    status = models.CharField(max_length=64,null=True)
+    #status = models.CharField(max_length=64,null=True)
